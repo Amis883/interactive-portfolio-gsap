695c873c12df2191c2202e80b74299411c8f4b96
Hoda ka <hodakakhki@Sinansoft.com>
2026-03-23 20:35:49 +0100

    feat: add multiple floating project cards layout

--- src/app/components/layout/Projects.tsx
+++ src/app/components/layout/Projects.tsx
@@ -3,32 +3,33 @@ import ProjectCard from "../ui/ProjectCard";
 export default function Projects() {
   return (
     <section className="py-40 relative overflow-hidden">
-      <div className="relative w-full h-[900px]">
-        {/* Card 1 */}
+      <div className="relative w-full h-[1200px]">
+        {/* LEFT SIDE */}
         <div className="absolute top-[0%] left-[10%]">
-          <ProjectCard
-            title="Modern Interior"
-            category="Design"
-            image="/p1.png"
-          />
+        {/* <div className="absolute top-[20%] left-[35%] rotate-[-5deg] scale-90"> */}
+          <ProjectCard title="Project 1" category="Design" image="/p1.png" />
         </div>
 
-        {/* Card 2 */}
-        <div className="absolute top-[15%] left-[40%]">
-          <ProjectCard
-            title="Fashion Campaign"
-            category="Photography"
-            image="/p2.png"
-          />
+        <div className="absolute top-[40%] left-[5%]">
+          <ProjectCard title="Project 2" category="UI" image="/p2.png" />
         </div>
 
-        {/* Card 3 */}
-        <div className="absolute top-[35%] left-[65%]">
-          <ProjectCard
-            title="Brand Identity"
-            category="Branding"
-            image="/p3.png"
-          />
+        {/* CENTER */}
+        <div className="absolute top-[20%] left-[35%]">
+          <ProjectCard title="Project 3" category="Branding" image="/p3.png" />
+        </div>
+
+        <div className="absolute top-[60%] left-[30%]">
+          <ProjectCard title="Project 4" category="Motion" image="/p1.png" />
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="absolute top-[10%] left-[65%]">
+          <ProjectCard title="Project 5" category="Web" image="/p2.png" />
+        </div>
+
+        <div className="absolute top-[50%] left-[70%]">
+          <ProjectCard title="Project 6" category="Creative" image="/p3.png" />
         </div>
       </div>
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import MainLayout from "./components/layout/MainLayout";
-import Hero from "./components/layout/Hero";
+// import Hero from "./components/layout/Hero";
 import Projects from "./components/layout/Projects";
 
 export default function Home() {
   return (
     <MainLayout>
-      <Hero />
+      {/* <Hero /> */}
       <Projects />
     </MainLayout>
   );
